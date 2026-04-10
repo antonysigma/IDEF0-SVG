9b771439152de4d7b4b325fb53d4059c45aa4f32
Antony Chan <antony.sigma@gmail.com>
2026-04-10 15:10:39 -0700

    Move function identifiers to bottom right corner of the boxes

--- lib/idef0/process_box.rb
+++ lib/idef0/process_box.rb
@@ -27,10 +27,31 @@ module IDEF0
     end
 
     def to_svg
+      raw_title = name.to_s.strip
+
+      if raw_title =~ /\A([^:]+):\s*(.*)\z/
+        fnc_id = Regexp.last_match(1).strip
+        fnc_name = Regexp.last_match(2).strip
+      else
+        fnc_id = "FNC-1"
+        fnc_name = raw_title
+      end
+
+      title = CentredLabel.new(
+        fnc_name,
+        Point.new(x1 + (width / 2), y1 + (height / 2) - 6)
+      )
+
+      identifier = RightAlignedLabel.new(
+        fnc_id.upcase,
+        Point.new(x2 - 8, y2 - 8)
+      )
+
       <<-XML
-<rect x='#{x1}' y='#{y1}' width='#{width}' height='#{height}' fill='lightyellow' stroke='black' filter="url(#f1fkw0u0qbu464)" />
-<text text-anchor='middle' x='#{x1 + (width / 2)}' y='#{y1 + (height / 2)}'>#{name}</text>
-XML
+    <rect x="#{x1}" y="#{y1}" width="#{width}" height="#{height}" fill="lightyellow" stroke="black" filter="url(#f1fkw0u0qbu464)" />
+    #{title.to_svg}
+    #{identifier.to_svg}
+    XML
     end
   end
 end
